a2040d3d41d825ecc2634c4b34a214d042f9fe51
royalcourtbd <sayedvai08@gmail.com>
2025-01-29 00:20:50 +0600

    🆙 Update Dependencies and App Version
    - 🔧 Upgrade Drift, build_runner, and related packages to latest versions
    - 🆙 Bump app version to 1.0.2+8
    - 🔄 Refactor database loader to use namespaced path_provider import
    - 🌐 Update Dart SDK version to 3.6.0

--- lib/data/services/database/database_loader.dart
+++ lib/data/services/database/database_loader.dart
@@ -1,6 +1,6 @@
 // lib/data/services/database_loader.dart
 import 'dart:io';
-import 'package:path_provider/path_provider.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
 import 'package:path/path.dart' as p;
 import 'package:drift/drift.dart';
 import 'package:drift/native.dart';
@@ -10,7 +10,7 @@ const String _dbName = "prayer_tracker.db";
 LazyDatabase loadDatabase() => LazyDatabase(() => _openConnection);
 
 Future<QueryExecutor> get _openConnection async {
-  final dbFolder = await getApplicationDocumentsDirectory();
+  final dbFolder = await path_provider.getApplicationDocumentsDirectory();
   final file = File(p.join(dbFolder.path, _dbName));
   return NativeDatabase.createInBackground(file);
 }
