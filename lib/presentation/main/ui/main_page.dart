7bcac18fe6f0bceeddfdfb9c646feb705890269a
royalcourtbd <126571880+devsmaxcode@users.noreply.github.com>
2025-02-27 14:43:54 +0600

    add event page in the bottom sheet

--- lib/presentation/main/ui/main_page.dart
+++ lib/presentation/main/ui/main_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:qibla_and_prayer_times/core/di/service_locator.dart';
 import 'package:qibla_and_prayer_times/core/external_libs/presentable_widget_builder.dart';
 import 'package:qibla_and_prayer_times/core/utility/utility.dart';
+import 'package:qibla_and_prayer_times/presentation/event/ui/event_page.dart';
 import 'package:qibla_and_prayer_times/presentation/main/presenter/main_presenter.dart';
 import 'package:qibla_and_prayer_times/presentation/main/presenter/main_ui_state.dart';
 import 'package:qibla_and_prayer_times/presentation/main/widgets/double_tap_back_to_exit_app.dart';
@@ -19,7 +20,7 @@ class MainPage extends StatelessWidget {
     // PrayerTimePage(),
     HomePage(),
     PrayerTrackerPage(),
-    ProfilePage(),
+    EventPage(),
     ProfilePage(),
     ProfilePage(),
   ];
@@ -39,7 +40,7 @@ class MainPage extends StatelessWidget {
             bottomNavigationBar: MainNavigationBar(
               selectedIndex: state.selectedBottomNavIndex,
               onDestinationSelected: (index) {
-                if (index >= 2 && index < 4) {
+                if (index == 3) {
                   showMessage(
                     message: 'Coming soon',
                   );
