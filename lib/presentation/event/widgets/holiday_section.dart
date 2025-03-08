c9fd6a48fac822aa3372842628865f4148e8e9e0
royalcourtbd <sayedvai08@gmail.com>
2025-03-09 00:20:13 +0600

    ✨ Feature: Add Navigation to Holiday Page in HolidaySection
    - 🚀 Implement navigation to HolidayPage when "See All" is tapped
    - 🔗 Import necessary utility and page dependencies
    - 🎨 Update SectionHeaderWithAction with context navigation extension

--- lib/presentation/event/widgets/holiday_section.dart
+++ lib/presentation/event/widgets/holiday_section.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:qibla_and_prayer_times/core/config/prayer_time_app_screen.dart';
 import 'package:qibla_and_prayer_times/core/static/ui_const.dart';
+import 'package:qibla_and_prayer_times/core/utility/utility.dart';
 import 'package:qibla_and_prayer_times/presentation/common/section_header_with_action.dart';
+import 'package:qibla_and_prayer_times/presentation/event/ui/holiday_page.dart';
 import 'package:qibla_and_prayer_times/presentation/event/widgets/govt_holiday_list.dart';
 
 class HolidaySection extends StatelessWidget {
@@ -22,7 +24,7 @@ class HolidaySection extends StatelessWidget {
           child: SectionHeaderWithAction(
             title: 'Govt Holidays',
             actionText: 'See All',
-            onActionTap: () {},
+            onActionTap: () => context.navigatorPush(HolidayPage()),
           ),
         ),
         gapH18,
