a1d36889383537bdbe47ea2bebe50bbf8c787f92
royalcourtbd <sayedvai08@gmail.com>
2025-03-27 19:43:26 +0600

    🔧 Chore: Refactor Database Loader for Environment Variable Integration
    - 🔄 Remove commented-out code and streamline the `database_loader.dart` file for improved clarity.
    - 🆕 Import `flutter_dotenv` to facilitate environment variable management.
    - 🧹 Ensure all changes align with Clean Architecture principles and maintain proper dependency registration in the service locator.

--- lib/data/services/database/database_loader.dart
+++ lib/data/services/database/database_loader.dart
@@ -1,20 +1,3 @@
-// // lib/data/services/database_loader.dart
-// import 'dart:io';
-// import 'package:path_provider/path_provider.dart' as path_provider;
-// import 'package:path/path.dart' as p;
-// import 'package:drift/drift.dart';
-// import 'package:drift/native.dart';
-
-// const String _dbName = "prayer_tracker.db";
-
-// LazyDatabase loadDatabase() => LazyDatabase(() => _openConnection);
-
-// Future<QueryExecutor> get _openConnection async {
-//   final dbFolder = await path_provider.getApplicationDocumentsDirectory();
-//   final file = File(p.join(dbFolder.path, _dbName));
-//   return NativeDatabase.createInBackground(file);
-// }
-
 import 'dart:io';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:path_provider/path_provider.dart';
