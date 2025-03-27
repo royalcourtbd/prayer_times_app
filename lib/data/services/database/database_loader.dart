e69764de848b331f634201e340c5dc4d989f0d34
royalcourtbd <sayedvai08@gmail.com>
2025-03-27 19:34:33 +0600

    🔧 Chore: Enhance Environment Variable Management and Database Initialization
    - 🆕 Add loading functionality for environment variables in `database_loader.dart` to improve configuration management.
    - 🔄 Update `getServerKeyToken` method in `GetServerKey` to utilize environment variables for secure credential handling.
    - 🧹 Ensure all changes align with Clean Architecture principles and maintain proper dependency registration in the service locator.

--- lib/data/services/database/database_loader.dart
+++ lib/data/services/database/database_loader.dart
@@ -16,12 +16,24 @@
 // }
 
 import 'dart:io';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as p;
 import 'package:drift/drift.dart';
 import 'package:drift/native.dart';
+import 'package:qibla_and_prayer_times/core/utility/logger_utility.dart';
 
 const String _dbName = "prayer_tracker.db";
+
+Future<void> loadEnv() async {
+  try {
+    await dotenv.load(fileName: '.env');
+    logDebugStatic('Env file loaded successfully', 'DatabaseLoader');
+  } catch (e) {
+    logErrorStatic('Error loading .env file: $e', 'DatabaseLoader');
+    throw Exception('Error loading .env file: $e');
+  }
+}
 
 LazyDatabase loadDatabase() {
   return LazyDatabase(() async {
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:qibla_and_prayer_times/core/di/service_locator.dart';
+import 'package:qibla_and_prayer_times/data/services/database/database_loader.dart';
 import 'package:qibla_and_prayer_times/domain/usecases/determine_first_run_use_case.dart';
 
 import 'package:qibla_and_prayer_times/presentation/prayer_times.dart';
@@ -17,6 +18,7 @@ Future<void> main() async {
 }
 
 Future<void> _initializeApp() async {
+  await loadEnv();
   await ServiceLocator.setUp();
 }
 
