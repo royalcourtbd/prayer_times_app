8e23c4aba35f49c75b172c7c5d88970a048c5caf
royalcourtbd <sayedvai08@gmail.com>
2025-03-28 05:11:27 +0600

    🔧 Chore: Improve Environment Variable Handling in Database Loader
    - 🔄 Refactor `database_loader.dart` to enhance environment variable loading functionality.
    - 🧹 Ensure all changes adhere to Clean Architecture principles and maintain proper dependency registration in the service locator.

---
+++ lib/data/datasources/remote/info_remote_data_sourse.dart
@@ -0,0 +1,23 @@
+import 'package:qibla_and_prayer_times/core/utility/trial_utility.dart';
+import 'package:qibla_and_prayer_times/data/services/backend_as_a_service.dart';
+
+class InfoRemoteDataSourse {
+  InfoRemoteDataSourse(this._backendAsAService);
+  final BackendAsAService _backendAsAService;
+
+  Future<void> getPromotionalMessage({
+    required void Function(String) onPromotionalMessage,
+  }) async {
+    _backendAsAService.getRemoteNotice(
+        onNotification: (Map<String, Object?> map) async {
+      await catchFutureOrVoid(() async {
+        final Map<String, Object?> remoteNotice = map.map(MapEntry.new);
+        final String? promotionalMessage =
+            remoteNotice['promotional_message'] as String?;
+        if (promotionalMessage != null) {
+          onPromotionalMessage(promotionalMessage);
+        }
+      });
+    });
+  }
+}
